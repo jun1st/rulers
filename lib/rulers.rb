06a5c55de707c8b2c797eae34df3137baf703002
feng-pro <jun1st.feng@gmail.com>
2014-03-22 23:10:23 +0800

    change

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,6 +4,7 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rules!"]]
     end
   end
